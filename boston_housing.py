b8500301b3859824321843b1dd556b49018cc93a
bywords <bywords.kor@gmail.com>
2015-12-15 11:21:56 +0900

    Updates based on review

--- boston_housing.py
+++ boston_housing.py
@@ -191,11 +191,15 @@ def fit_predict_model(city_data):
     # Fit the learner to the training data to obtain the best parameter set
     print "Final Model: "
     print reg.fit(X, y)
-    print reg.best_params_
+
+    # Get the best estimator
+    best_fit = reg.best_estimator_
+    print "Best Fit: "
+    print best_fit
 
     # Use the model to predict the output of a particular sample
     x = [[11.95, 0.00, 18.100, 0, 0.6590, 5.6090, 90.00, 1.385, 24, 680.0, 20.20, 332.09, 12.13]]
-    y = reg.predict(x)
+    y = best_fit.predict(x)
     print "House: " + str(x)
     print "Prediction: " + str(y)
 
